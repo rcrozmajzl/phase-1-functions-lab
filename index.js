3cd8a49cdae10af9fb623c2f60f6010ad53cc490
Rebecca Rozmajzl <rebecca.rozmajzl@gmail.com>
2022-02-07 12:14:48 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const destination = 42;
+
+function distanceFromHqInBlocks (start) {
+    if (destination > start) {
+        return (destination - start);
+    }
+    else {
+        return (start - destination);
+    }
+}
+
+function distanceFromHqInFeet (start) {
+    return (distanceFromHqInBlocks(start) * 264);
+}
+
+function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+        return ((destination - start) * 264);
+    }
+    else {
+        return ((start - destination) * 264);
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * 0.02);
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
